3d7f7b09162bbc0989ee46546239c812bf4303ec
Bruno Teixeira <brunombteixeira@Brunos-MacBook-Air.local>
2026-03-19 17:55:08 +0000

    feat: add InteriorViewer placeholder component

---
+++ src/components/configurator/InteriorViewer.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+interface InteriorViewerProps {
+  images?: string[]
+}
+
+export default function InteriorViewer({ images: _ }: InteriorViewerProps) {
+  return (
+    <div className="absolute inset-0 w-full h-full">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/images/nissan-leaf-lights.jpg"
+        alt="Interior view"
+        className="w-full h-full object-cover"
+        draggable={false}
+      />
+    </div>
+  )
+}
